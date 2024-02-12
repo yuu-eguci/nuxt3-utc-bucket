b7156587974f24178ce9debfe2592c5892188d37
yuu-eguci <yuu.eguci@gmail.com>
2024-02-12 18:58:34 +0900

    🔧 nuxt.config に app.baseURL を追加 (GitHub Pages のための準備)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    // https://nuxt.com/docs/api/nuxt-config#baseurl
+    // Default: '/'
+    baseURL: process.env.ROUTER_BASE || '/',
+  },
+
   devtools: { enabled: true }
 })
