4d9c35a1eb99675de7eb5c8498c41b264bfd9a3f
yuu-eguci <yuu.eguci@gmail.com>
2024-02-12 19:15:22 +0900

    🔧 favicon.ico を `${process.env.ROUTER_BASE}favicon.ico` で設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ export default defineNuxtConfig({
     // https://nuxt.com/docs/api/nuxt-config#baseurl
     // Default: '/'
     baseURL: process.env.ROUTER_BASE || '/',
+
+    // https://nuxt.com/docs/getting-started/seo-meta
+    head: {
+      title: 'Nuxt3 UTC Bucket',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: `${process.env.ROUTER_BASE}favicon.ico` },
+      ],
+    }
   },
 
   devtools: { enabled: true }
