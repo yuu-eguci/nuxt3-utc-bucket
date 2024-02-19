8843ad5401531ae5ae27aee9a71dbb04f02a538b
yuu-eguci <yuu.eguci@gmail.com>
2024-02-19 18:12:25 +0900

    💡 nuxt.config.ts の i18n や、 defineI18nConfig を、 VSCode が認識するまでの NOTE

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,5 +1,16 @@
 // NOTE: @nuxtjs/i18n 公式ページのまま。
 //       https://i18n.nuxtjs.org/docs/getting-started/usage
+// NOTE: 導入時に、 VSCode がエラーを出してくることがあった。
+//       Cannot find name 'defineI18nConfig'. Did you mean 'defineNuxtConfig'? ts(2552)
+//       tsconfig.json に "types": ["@nuxt/types"] を追加することで解決した。
+//         "compilerOptions": {
+//           "types": [
+//               "@nuxtjs/i18n",
+//           ]
+//         }
+//       けど実は、 "extends": "./.nuxt/tsconfig.json" の中でちゃんと
+//       "../node_modules/@nuxtjs/i18n/runtime" が読まれており、
+//       時間を置いたら VSCode はちゃんとこれらを認識してくれるのかもしれない。
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
