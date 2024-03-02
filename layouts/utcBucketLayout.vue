f1b71a9452a894cfaedb1551fe5f5a22bb53221a
yuu-eguci <yuu.eguci@gmail.com>
2024-03-02 17:25:45 +0900

    ✨ nuxt-utc-bucket をコピーする - Layout (Vuetify3のドキュメントを観ながら)

---
+++ layouts/utcBucketLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <!--
+    https://vuetifyjs.com/en/api/v-app/
+    https://vuetifyjs.com/en/features/theme/
+  -->
+  <v-app theme="dark">
+    <!--
+      https://vuetifyjs.com/en/api/v-app-bar/
+    -->
+    <v-app-bar>
+      <template #prepend>
+        <v-icon
+          color="success"
+          icon="mdi-pail-outline"
+          size="large"
+        />
+      </template>
+    </v-app-bar>
+    <slot />
+    <!--
+      https://vuetifyjs.com/en/api/v-footer/
+      - app false: ページ最下部
+    -->
+    <v-footer
+      :app="false"
+    >
+      <span>
+        &copy; {{ new Date().getFullYear() }}
+      </span>
+      <v-spacer />
+      <a href="https://github.com/yuu-eguci/nuxt3-utc-bucket">
+        <v-icon
+          icon="mdi-github"
+          size="large"
+        />
+      </a>
+    </v-footer>
+  </v-app>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+// NOTE: この component の layout とかを設定するやつ。
+definePageMeta({
+  layout: 'utc-bucket-layout',
+})
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
